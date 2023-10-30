c8459a91b16816f521d7faf8cfd6ebff213f152a
Kate.Su <kate.su@remotes.com.tw>
2023-10-30 14:34:57 +0800

    add submitWhereToGo

--- src/main.js
+++ src/main.js
@@ -12,3 +12,13 @@ window.addEventListener('scroll', function () {
 
   slide1.style.backgroundPosition = `0px -${window.pageYOffset * 1.5}px`;
 });
+
+function submitWhereToGo() {
+  const result = document.querySelector('#result');
+  result.classList.remove('hidden');
+  // result.classList.add('block');
+  const randomNum = Math.floor(Math.random() * (2 - 0 + 1) + 0);
+  const resultContent = document.querySelector('#resultContent');
+  const resultArr = ['法國聖米歇爾山', '德國新天鵝堡', '慕尼黑二日遊'];
+  resultContent.innerHTML = resultArr[randomNum];
+}
